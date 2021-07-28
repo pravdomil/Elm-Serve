5202432c7d238c6558cc003cd9b46e044c1662e2
pravdomil <2387356+pravdomil@users.noreply.github.com>
2021-07-28 22:16:54 +0200

    Main module

--- src/ElmServe/Main.elm
+++ src/ElmServe/Main.elm
@@ -1,5 +1,12 @@
 module ElmServe.Main exposing (..)
 
+import ElmServe.Options as Options exposing (Options)
+import Interop.JavaScript as JavaScript
+import Json.Decode as Decode
+import Parser
+import Parser.DeadEnd as DeadEnd
+import Task exposing (Task)
+
 
 main : Program () Model Msg
 main =
@@ -15,13 +22,32 @@ main =
 
 
 type alias Model =
-    {}
+    { options : Maybe Options
+    }
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( {}
-    , Cmd.none
+    let
+        getArgs : Cmd Msg
+        getArgs =
+            JavaScript.run "process.argv"
+                |> JavaScript.decode (Decode.list Decode.string)
+                |> Task.mapError JavaScriptError
+                |> Task.andThen
+                    (\v ->
+                        case Options.parse (List.drop 2 v) of
+                            Ok vv ->
+                                Task.succeed vv
+
+                            Err vv ->
+                                Task.fail (CannotParseOptions vv)
+                    )
+                |> Task.attempt GotOptions
+    in
+    ( { options = Nothing
+      }
+    , getArgs
     )
 
 
@@ -30,16 +56,60 @@ init _ =
 
 
 type Msg
-    = NothingHappened
+    = GotOptions (Result Error Options)
+    | TaskDone (Result Error ())
+    | NothingHappened
+
+
+type Error
+    = JavaScriptError JavaScript.Error
+    | CannotParseOptions (List Parser.DeadEnd)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        GotOptions a ->
+            case a of
+                Ok b ->
+                    ( { model | options = Just b }
+                    , log (Options.toString b)
+                        |> Task.attempt TaskDone
+                    )
+
+                Err b ->
+                    ( model
+                    , Task.fail b
+                        |> Task.attempt TaskDone
+                    )
+
+        TaskDone a ->
+            case a of
+                Ok _ ->
+                    ( model
+                    , Cmd.none
+                    )
+
+                Err b ->
+                    ( model
+                    , exitWithMessageAndCode (errorToString b) 1
+                        |> Task.attempt (always NothingHappened)
+                    )
+
         NothingHappened ->
             ( model
             , Cmd.none
             )
+
+
+errorToString : Error -> String
+errorToString a =
+    case a of
+        JavaScriptError b ->
+            JavaScript.errorToString b
+
+        CannotParseOptions b ->
+            "Cannot decoder options because:\n" ++ DeadEnd.toString b
 
 
 
@@ -49,3 +119,21 @@ update msg model =
 subscriptions : Model -> Sub Msg
 subscriptions _ =
     Sub.none
+
+
+
+--
+
+
+log : String -> Task Error ()
+log _ =
+    JavaScript.run "console.log(_v0)"
+        |> Task.mapError JavaScriptError
+        |> Task.map (\_ -> ())
+
+
+exitWithMessageAndCode : String -> Int -> Task Error ()
+exitWithMessageAndCode _ _ =
+    JavaScript.run "(() => { console.error(_v0); process.exit(_v1); })()"
+        |> Task.mapError JavaScriptError
+        |> Task.map (\_ -> ())
