50a942b0306a07a12ca4d131175852702837debd
pravdomil <2387356+pravdomil@users.noreply.github.com>
2022-02-01 00:46:55 +0100

    docs

--- bin/build.sh
+++ bin/build.sh
@@ -12,7 +12,7 @@ cd "${0%/*}/.."
 # Have dependencies from npm ready.
 npm i
 
-# Compile our app.
+# Compile application.
 elm make src/Main.elm --output bin/elm-serve.js --optimize
 elm-ffi bin/elm-serve.js --run --shebang
 mv bin/elm-serve.js bin/elm-serve
